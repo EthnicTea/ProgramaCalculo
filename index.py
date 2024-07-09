0f9a27416c14be1752444da8bfe829b10b9e1c05
EthnicTea <rensito.9000@hotmail.coM>
2024-07-09 19:09:46 -0400

    Limpiando el código

--- index.py
+++ index.py
@@ -3,19 +3,19 @@ import math
 import sympy as sp 
 #import numpy as np
 while True:
-    system("cls");
-    print("");
-    print("=======================================");
-    print(".... Bienvenido a nuestro programa ....");
-    print("=======================================");
-    print("");
+    system("cls")
+    print("")
+    print("=======================================")
+    print(".... Bienvenido a nuestro programa ....")
+    print("=======================================")
+    print("")
     print("=================================")
-    print(".... Menu de Calculos ....");
-    print("=================================");
-    print("  1.-  Calcular la Utilidad  ");
-    print("  2.-  Calcular Utilidad Maxima  ");
-    print("  3.-  Salir del Programa   ");
-    print("=================================");
+    print(".... Menu de Calculos ....")
+    print("=================================")
+    print("  1.-  Calcular la Utilidad  ")
+    print("  2.-  Calcular Utilidad Maxima  ")
+    print("  3.-  Salir del Programa   ")
+    print("=================================")
     print("");
     Opc = None  # Inicializamos Opc con None para asegurar que entre al bucle la primera vez
     while Opc not in [1, 2, 3]:  # Mientras Opc no sea 1 o 2, repetir la solicitud
@@ -27,16 +27,16 @@ while True:
             print("Por favor ingrese una opción válida (1-3)")
     print("")
     if(Opc == 1):
-        print("===============================================");
-        print(".... Haz seleccionado Calcular la Utilidad ....");
-        print("===============================================");
-        print("");
-        print("===================================");
+        print("===============================================")
+        print(".... Haz seleccionado Calcular la Utilidad ....")
+        print("===============================================")
+        print("")
+        print("===================================")
         while True:
             try:
                 Valor1 = input("Ingrese un valor: ").strip()  # Pedimos el valor y eliminamos espacios en blanco alrededor
                 if Valor1 == "":  # Si el usuario deja el campo en blanco
-                    raise ValueError("No ha ingresado ningún valor");
+                    raise ValueError("No ha ingresado ningún valor")
                 x = float(Valor1)  # Intentamos convertir el valor a un número flotante
                 while x < 0 or x == 0: # Si el usuario inserta valores negativos o 0
                     Valor1 = input("¡¡¡No ingrese valores menjores a 0 o negativos!!!: ").strip()
@@ -44,29 +44,29 @@ while True:
                 # Si llegamos hasta aquí, hemos obtenido con éxito un número
                 break  # Salimos del bucle while porque tenemos el valor correcto
             except ValueError as e:
-                print(f"Error: {e}. Por favor ingrese un número válido.");
+                print(f"Error: {e}. Por favor ingrese un número válido.")
         Venta = (5500*x);
-        Costo = (214.812*x**2+1395.14*math.sqrt(x));
-        Utilidad = Venta-Costo;
-        print("===================================");
-        print("");
-        print("======================================");
+        Costo = (214.812*x**2+1395.14*math.sqrt(x))
+        Utilidad = Venta-Costo
+        print("===================================")
+        print("")
+        print("======================================")
         print(".......... MUESTRA DE DATOS ..........")
-        print("======================================");
-        print(".... La venta es    : ",Venta,"....");
-        print(".... El costo es    : ",Costo,"....");
-        print(".... La utilidad es : ",Utilidad,"....");
-        print("======================================");
+        print("======================================")
+        print(".... La venta es    : ",Venta,"....")
+        print(".... El costo es    : ",Costo,"....")
+        print(".... La utilidad es : ",Utilidad,"....")
+        print("======================================")
     if(Opc == 2):
-        print("===============================================");
-        print(".... Haz seleccionado Calcular la Utilidad Maxima ....");
-        print("===============================================");
-        print("");
-        print("===================================");
+        print("===============================================")
+        print(".... Haz seleccionado Calcular la Utilidad Maxima ....")
+        print("===============================================")
+        print("")
+        print("===================================")
         x = sp.Symbol('x')
-        Venta = (5500*x);
-        Costo = (214.812*x**2+1395.14*sp.sqrt(x));
-        Utilidad = (Venta-Costo);
+        Venta = (5500*x)
+        Costo = (214.812*x**2+1395.14*sp.sqrt(x))
+        Utilidad = (Venta-Costo)
         derivada = sp.diff(Utilidad, x)
 
         # Solucionar la derivada para encontrar los puntos críticos
@@ -78,23 +78,23 @@ while True:
         for punto, utilidad in utilidad_en_puntos:
             print(f'Punto crítico: x = {punto}')
         # Mostrar resultados
-        print("===================================");
-        print("");
-        print("======================================");
+        print("===================================")
+        print("")
+        print("======================================")
         print(".......... MUESTRA DE DATOS ..........")
-        print("======================================");
-        print(".... La venta es    : ",Venta,"....");
-        print(".... El costo es    : ",Costo,"....");
-        print(".... La utilidad es : ",Utilidad,"....");
-        print(".... La derivada es : ",derivada,"....");
-        print(".... La Utilidad maxima es : ",punto,"....");
-        print("======================================");
+        print("======================================")
+        print(".... La venta es    : ",Venta,"....")
+        print(".... El costo es    : ",Costo,"....")
+        print(".... La utilidad es : ",Utilidad,"....")
+        print(".... La derivada es : ",derivada,"....")
+        print(".... La Utilidad maxima es : ",punto,"....")
+        print("======================================")
     if(Opc == 3):
-        print("===================================");
-        print("Haz seleccionado salir del programa");
-        print("          ¡¡Nos vemos!!            ");
-        print("===================================");
-        break;
+        print("===================================")
+        print("Haz seleccionado salir del programa")
+        print("          ¡¡Nos vemos!!            ")
+        print("===================================")
+        break
     continuar = input("\n¿Desea realizar otra operación? (s/cualquier tecla): ").strip().lower()
     if continuar != 's':
         break  # Salir del bucle principal si la respuesta no es 's'
